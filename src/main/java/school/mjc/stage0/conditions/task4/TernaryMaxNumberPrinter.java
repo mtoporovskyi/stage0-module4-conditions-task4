0449294307ca4c9b0508fd81dd38c07ee8e9354e
Myroslav Toporovskyi <mtoporovskyy@gmail.com>
2023-12-10 18:12:20 +0200

    finished stage0-module4-conditions-task4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int bigger = first > second ? first : second;
+        System.out.println(bigger > third ? bigger : third);
     }
 }
